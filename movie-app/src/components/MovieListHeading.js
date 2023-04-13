6f8550ba2c0c753f2e100f5e36c81cc3278d4b23
p4trykk <128367670+p4trykk@users.noreply.github.com>
2023-04-13 19:28:03 +0200

    Adding font

--- movie-app/src/components/MovieListHeading.js
+++ movie-app/src/components/MovieListHeading.js
@@ -5,7 +5,7 @@ import { BiMoviePlay } from 'react-icons/bi';
 const MovieListHeading = (props) => {
 	return (
 		<div className='col'>
-			<h1>
+			<h1 className='col1'>
 				{props.heading}
 				<BiMoviePlay className="logo1"/>
 			</h1>
