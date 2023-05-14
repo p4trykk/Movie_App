62f04d06c0af6ffae35b10e4400af34713c35611
p4trykk <128367670+p4trykk@users.noreply.github.com>
2023-05-14 19:48:33 +0200

    update_fav_list_and_details_screen
    upgrade adding to favorites list (add and remove) and upload details screen (not dynamic routes yet)

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieList = (props) => {
 	const FavouriteComponent = props.favouriteComponent;
@@ -7,7 +8,9 @@ const MovieList = (props) => {
 		<>
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
+					<Link to="/details">
+						<img src={movie.Poster} alt='movie'></img>
+					</Link>
 					<div
 						onClick={() => props.handleFavouritesClick(movie)}
 						className='overlay d-flex align-items-center justify-content-center'
